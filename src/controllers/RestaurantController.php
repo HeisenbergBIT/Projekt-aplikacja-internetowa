e37b40bf2539b870818d4b7bd2349dc1994008b6
Mikołaj Grucel <73535344+HeisenbergBIT@users.noreply.github.com>
2022-02-07 15:48:22 +0100

    upload restaurant

--- src/controllers/RestaurantController.php
+++ src/controllers/RestaurantController.php
@@ -1,12 +1,45 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../models/Restaurant.php';
 
 
 class RestaurantController extends AppController{
 
-        public function addRestaurant(){
+    const MAX_FILE_SIZE = 1024*1024;
+    const SUPPORTED_TYPES = ['image/png', 'image/jpeg'];
+    const UPLOAD_DIRECTORY = '/../public/uploads/';
 
-            $this->render('add-restaurant');
+    private $messages = [];
+
+    public function addRestaurant(){
+
+        if ($this->isPost() && is_uploaded_file($_FILES['file']['tmp_name']) && $this->validate($_FILES['file'])){
+            move_uploaded_file(
+                $_FILES['file']['tmp_name'],
+                dirname(__DIR__).self::UPLOAD_DIRECTORY.$_FILES['file']['name']
+            );
+
+            $restaurant = new Restaurant($_POST['title'], $_POST['description'], $_FILES['file']['name']);
+
+            return $this->render('restaurants', ['restaurant' => $restaurant]);
+
         }
+
+        return $this->render('add-restaurant', ['messages' => $this->messages]);
+    }
+
+    private function validate(array $file): bool
+    {
+        if ($file['size'] > self::MAX_FILE_SIZE) {
+            $this->messages[] = 'File is too large';
+            return false;
+        }
+
+        if (!isset($file['type']) || !in_array($file['type'], self::SUPPORTED_TYPES)) {
+            $this->messages[] = 'File type is not supported';
+            return false;
+        }
+        return true;
+    }
 }
