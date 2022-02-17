fd299a56688f35e5b26eb3d2fbc81e889ab2b957
Mikołaj Grucel <73535344+HeisenbergBIT@users.noreply.github.com>
2022-02-17 17:12:35 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'rqlpqtgyjvrlfc';
-const PASSWORD = '30b76ad1fd7a81698977148cbb99f7260a68818e8b6163154d93fa7623f54eb2';
-const HOST = 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd97l70okv03vac';
